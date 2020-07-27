e379f855706384cfaf54a7559565b858d9b83323
Alexey Marchenko <leha.google@mail.ru>
2020-07-27 19:32:34 +0300

    verskta palich

--- src/js/_parts/_sliders.js
+++ src/js/_parts/_sliders.js
@@ -5,49 +5,75 @@ $(document).ready(function () {
 	//	$('.js-productSlider .swiper-pagination').hide();
 	//	$('.js-productSlider .slider-button').hide();
 	//}
-	var $hero = $(this),
-		$slides = $hero.find(".main-slider .swiper-slide");
 
-	var heroSwiper = new Swiper('.main-slider .swiper-container', {
-		effect: "slide",
-		speed: 250,
-		//spaceBetween: 20,
-		loop: true,
-		autoplay: {
-			delay: 5000,
-		},
-		delay: 4000,
-		navigation: {
-			nextEl: ".main-slider .swiper-button-next",
-			prevEl: ".main-slider .swiper-button-prev"
+	$('.section-slider').each(function () {
+		//var $hero = $(this),
+		var data_slider_name = $(this).find('.swiper-container').data('slider-name');
+		//console.log(data_slider_name);
+		var $slides = $(this).find('[data-slider-name="'+data_slider_name+'"] .swiper-slide');
+
+		if (data_slider_name=="stock-slider"){
+			var heroSwiper = new Swiper('[data-slider-name="'+data_slider_name+'"].swiper-container', {
+				effect: "slide",
+				speed: 250,
+				slidesPerView:'auto',
+				spaceBetween: 0,
+				loop: true,
+				autoplay: {
+					delay: 5000,
+				},
+				delay: 4000,
+				navigation: {
+					nextEl: '[data-slider-name="'+data_slider_name+'"] .swiper-button-next',
+					prevEl: '[data-slider-name="'+data_slider_name+'"] .swiper-button-prev'
+				}
+			});
+		} else {
+
+			var heroSwiper = new Swiper('[data-slider-name="'+data_slider_name+'"].swiper-container', {
+				effect: "slide",
+				speed: 250,
+				spaceBetween: 0,
+				loop: true,
+				autoplay: {
+					delay: 5000,
+				},
+				delay: 4000,
+				navigation: {
+					nextEl: '[data-slider-name="'+data_slider_name+'"] .swiper-button-next',
+					prevEl: '[data-slider-name="'+data_slider_name+'"] .swiper-button-prev'
+				}
+			});
 		}
+
+		var $progress = $('[data-slider-name="'+data_slider_name+'"] .progress'),
+			$progressIndex = $progress.find("span.index"),
+			$progressSlides = $progress.find("span.slides"),
+			$progressBar = $progress.find(".progress-bar .back");
+
+		var slides = $slides.length,
+			progressSlides = slides;
+
+		if (progressSlides < 10) {
+			progressSlides = "0" + progressSlides;
+		}
+
+		$progressSlides.text(progressSlides);
+
+		heroSwiper.on('slideChange', function () {
+			var progressIndex = heroSwiper.realIndex + 1,
+				progress = heroSwiper.realIndex / (slides - 1) * 100;
+
+			if (progressIndex < 10) {
+				progressIndex = "0" + progressIndex;
+			}
+
+			$progressIndex.text(progressIndex);
+			$progressBar.css("width", progress + "%");
+		});
 	});
 
-	var $progress = $(".progress"),
-		$progressIndex = $progress.find("span.index"),
-		$progressSlides = $progress.find("span.slides"),
-		$progressBar = $progress.find(".progress-bar .back");
 
-	var slides = $slides.length,
-		progressSlides = slides;
-
-	if (progressSlides < 10) {
-		progressSlides = "0" + progressSlides;
-	}
-
-	$progressSlides.text(progressSlides);
-
-	heroSwiper.on('slideChange', function () {
-		var progressIndex = heroSwiper.realIndex + 1,
-			progress = heroSwiper.realIndex / (slides - 1) * 100;
-
-		if (progressIndex < 10) {
-			progressIndex = "0" + progressIndex;
-		}
-
-		$progressIndex.text(progressIndex);
-		$progressBar.css("width", progress + "%");
-	});
 
 
 	/* в списке товаров */
