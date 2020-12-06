bdbcd86b3bf7fdde7a7af7d9e794c42851352d81
Karaulovskin <karaulovskin@yandex.ru>
2020-12-06 11:28:30 +0300

    парвки

--- src/js/_parts/_delivery-special.js
+++ src/js/_parts/_delivery-special.js
@@ -2,20 +2,25 @@ $(document).ready(function () {
 	let deliveryColor = $('body').data('color'),
 		priceSale = $('.products__item__price__sale');
 
-	$('.icon-delivery-arrow, .icon-delivery-address-point').css('fill',deliveryColor);
+	window.customColor = function() {
+		$('.icon-delivery-arrow, .icon-delivery-address-point').css('fill',deliveryColor);
 
-	$('.header-delivery .button, .delivery-label').css('background-color',deliveryColor);
-	$('.delivery-special .button').css({'background-color':deliveryColor, 'color': 'white'});
-	$('.header-cart__number').css({'color': deliveryColor});
+		$('.header-delivery .button, .delivery-label').css('background-color',deliveryColor);
+		$('.delivery-special .button').css({'background-color':deliveryColor, 'color': 'white'});
+		$('.header-cart__number').css({'color': deliveryColor});
 
-	priceSale.each(function () {
-		$(this)
-			.siblings('.products__item__price__normal')
-			.css({'color': deliveryColor});
-	});
+		priceSale.each(function () {
+			$(this)
+				.siblings('.products__item__price__normal')
+				.css({'color': deliveryColor});
+		});
 
-	if($(window).width() < 768){
-		$('.header-cart__icon .icon-cart').css('fill',deliveryColor);
-		$('.header-cart__number').css('background-color',deliveryColor);
+		if($(window).width() < 768){
+			$('.header-cart__icon .icon-cart').css('fill',deliveryColor);
+			$('.header-cart__number').css('background-color',deliveryColor);
+		}
 	}
+
+	window.customColor();
+
 });
