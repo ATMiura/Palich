6214a4a46b88e5856bee325b4c65430395a5bf80
ATMiura <leha.google@mail.ru>
2020-12-10 20:48:58 +0300

    before pull

--- build___/js/main.js
+++ build___/js/main.js
@@ -341,6 +341,7 @@ $(document).ready(function () {
   var deliveryColor = $('body').data('color');
   $('.icon-delivery-arrow, .icon-delivery-address-point').css('fill', deliveryColor);
   $('.header-delivery .button, .delivery-label').css('background-color', deliveryColor);
+  $('.delivery-special .button').css('background-color', deliveryColor);
 
   if ($(window).width() < 768) {
     $('.header-cart__icon .icon-cart').css('fill', deliveryColor);
--- src/js/_parts/_delivery-special.js
+++ src/js/_parts/_delivery-special.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
 	$('.icon-delivery-arrow, .icon-delivery-address-point').css('fill',deliveryColor);
 
 	$('.header-delivery .button, .delivery-label').css('background-color',deliveryColor);
+	$('.delivery-special .button').css('background-color',deliveryColor);
 
 	if($(window).width() < 768){
 		$('.header-cart__icon .icon-cart').css('fill',deliveryColor);
